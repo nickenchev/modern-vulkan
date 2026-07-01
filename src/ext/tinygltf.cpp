54b18df9b3a557dbbcbfbfc5845a4454d5532451
Nick Enchev <nick@ensoft.io>
2026-07-01 10:10:20 +0300

    Re-added stb image macro

--- src/ext/tinygltf.cpp
+++ src/ext/tinygltf.cpp
@@ -1,6 +1,6 @@
 #define TINYGLTF3_IMPLEMENTATION
 #define TINYGLTF3_ENABLE_FS          // enable file I/O
-//#define TINYGLTF3_ENABLE_STB_IMAGE   // enable image decoding
+#define TINYGLTF3_ENABLE_STB_IMAGE   // enable image decoding
 #include "tiny_gltf_v3.h"
 
 #define STB_IMAGE_IMPLEMENTATION
